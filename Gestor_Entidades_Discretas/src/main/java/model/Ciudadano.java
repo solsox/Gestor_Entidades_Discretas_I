b50f325e10f9fee13e6bb1e38f67eb3017b04cb6
VaronDev <varonssmuel6@gmail.com>
2026-04-08 17:47:20 -0500

    Add feature model

--- Gestor_Entidades_Discretas/src/main/java/model/Ciudadano.java
+++ Gestor_Entidades_Discretas/src/main/java/model/Ciudadano.java
@@ -1,4 +1,29 @@
 package model;
 
 public class Ciudadano {
+        private String cedula;
+        private String nombre;
+        private String apellido;
+
+        public Ciudadano(String cedula, String nombre, String apellido) {
+            this.cedula = cedula;
+            this.nombre = nombre;
+            this.apellido = apellido;
+        }
+
+        public String getCedula() {
+            return cedula;
+        }
+
+        public void setCedula(String cedula){
+            this.cedula = cedula;
+        }
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+            Ciudadano ciudadano = (Ciudadano) obj;
+            return cedula.equals(ciudadano.cedula);
+
+        }
 }
