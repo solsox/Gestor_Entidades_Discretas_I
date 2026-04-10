8276a49b5e07d87d76508d03f07deb9e09d6ffe8
Santiago Millan  Sanchez <1105369961@u.icesi.edu.co>
2026-04-10 07:39:23 -0500

    commit estructuras lista

--- Gestor_Entidades_Discretas/src/main/java/Estructuras/ListaEnlazada.java
+++ Gestor_Entidades_Discretas/src/main/java/Estructuras/ListaEnlazada.java
@@ -1,4 +1,57 @@
 package Estructuras;
 
-public class ListaEnlazada {
+public class ListaEnlazada<T> {
+
+    private Nodo<T> cabeza;
+    public void agregar(T d) {
+        Nodo<T> nuevo = new Nodo<>(d);
+        if (cabeza == null) {
+            cabeza = nuevo;
+        } else {
+            Nodo<T> aux = cabeza;
+            while (aux.sig != null) aux = aux.sig;
+            aux.sig = nuevo;
+        }
+    }
+
+    public T obtener(int i) {
+        Nodo<T> aux = cabeza;
+        int cont = 0;
+        while (aux != null) {
+            if (cont == i) return aux.dato;
+            aux = aux.sig;
+            cont++;
+        }
+        return null;
+    }
+
+    public T buscar(T d) {
+        Nodo<T> aux = cabeza;
+        while (aux != null) {
+            if (aux.dato.equals(d)) return aux.dato;
+            aux = aux.sig;
+        }
+        return null;
+    }
+
+    public T eliminar(T d) {
+        if (cabeza == null) return null;
+
+        if (cabeza.dato.equals(d)) {
+            T borrado = cabeza.dato;
+            cabeza = cabeza.sig;
+            return borrado;
+        }
+
+        Nodo<T> aux = cabeza;
+        while (aux.sig != null) {
+            if (aux.sig.dato.equals(d)) {
+                T borrado = aux.sig.dato;
+                aux.sig = aux.sig.sig;
+                return borrado;
+            }
+            aux = aux.sig;
+        }
+        return null;
+    }
 }
--- Gestor_Entidades_Discretas/src/main/java/Estructuras/Nodo.java
+++ Gestor_Entidades_Discretas/src/main/java/Estructuras/Nodo.java
@@ -1,4 +1,11 @@
 package Estructuras;
 
-public class Nodo {
+public class Nodo<T> {
+    public T dato;
+    public Nodo<T> sig;
+
+    public Nodo(T contenido) {
+        this.dato = contenido;
+        this.sig = null;
+    }
 }
